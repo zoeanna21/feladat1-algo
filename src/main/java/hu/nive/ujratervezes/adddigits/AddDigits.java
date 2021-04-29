6331a20f6ced2bb011ddf0aed0a735713930404e
zoe <gregor.ancsi@gmail.com>
2021-04-29 11:46:07 +0200

    .

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,19 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public String addDigits(String input) {
+
+        if () {
+            return null;
+        } else return -1;
+
+
+    }
+
+
 }
+
+//Hozd létre az AddDigits osztályt, és annak az addDigits(String input) metódusát.
+//
+//A metódus feladata, hogy a paraméterben kapott Stringben található összes számjegyet adja össze, és ennek az összegét adja vissza. Ha a bemeneti String üres, vagy null, adjon vissza -1 -et.
